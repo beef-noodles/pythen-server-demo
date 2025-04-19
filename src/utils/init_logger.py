78489f8bcd0058b9631e4554f166b9fabe87328b
guzhongren <guzhongren@live.cn>
2025-04-19 18:34:58 +0800

    fix: log

--- src/utils/init_logger.py
+++ src/utils/init_logger.py
@@ -3,7 +3,6 @@ import logging.config
 import os
 import yaml
 
-from colorlog import ColoredFormatter
 
 def init_logger():
     log_dir = "logs"
@@ -13,24 +12,3 @@ def init_logger():
     with open("logging.yaml", "rt") as f:
         config = yaml.safe_load(f.read())
         logging.config.dictConfig(config)
-
-    # Remove all existing handlers
-    root_logger = logging.getLogger()
-    for handler in root_logger.handlers[:]:
-        root_logger.removeHandler(handler)
-
-    formatter = ColoredFormatter(
-        "%(log_color)s%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        datefmt=None,
-        reset=True,
-        log_colors={
-            "DEBUG": "cyan",
-            "INFO": "green",
-            "WARNING": "yellow",
-            "ERROR": "red",
-            "CRITICAL": "red,bg_white",
-        },
-    )
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    root_logger.addHandler(handler)
